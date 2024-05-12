6b2ce29142ae2d7c68313301b93a315cb716298c
Doc Obura <docdaystar@gmail.com>
2024-05-13 01:45:42 +0300

    add doc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navbar from './components/navbar';
+import About from './components/Menu';
+import Cart from './components/Cart';
 
-function App() {
+import "./App.css";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <h1>Food Palace</h1>
+        <div>
+        </div>
+
+        <Route path="/about" component={About} />
+        <Route path="/cart" component={Cart} />
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/components/navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styles/navbar.css'; // Import CSS file
+
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">About</a></li>
+        <li><a href="/about">Home</a></li>
+        <li><a href="/cart">Contact Us</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
